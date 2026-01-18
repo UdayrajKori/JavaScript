06fde779c70cf3c29f2062c825296d2789338895
udayrajkori <uday.kori24@radiantcollege.edu.np>
2026-01-18 12:59:46 +0545

    To complete the assignment of Assignment operator

---
+++ 03_Operators/Assignment_Operator/Assignments/01_AddSubMulDiv.js
@@ -0,0 +1,20 @@
+
+// Assignment Operators
+
+let a = 10;
+let b = 5;
+
+a += b; // a = a + b
+console.log("After a += b, a = ", a);
+
+a -= b; // a = a - b
+console.log("After a -= b, a = ", a);
+
+a *= b; // a = a * b
+console.log("After a *= b, a = ", a);
+
+a /= b; // a = a / b
+console.log("After a /= b, a = ", a);
+
+a %= b; // a = a % b
+console.log("After a %= b, a = ", a);
